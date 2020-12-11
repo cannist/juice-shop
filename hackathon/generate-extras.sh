1df41b200d678e5867fb68c627b8913cc946dc04
Thomas Horstmeyer <cannist@github.com>
2020-12-11 14:49:17 +0000

    Try different path

--- hackathon/generate-extras.sh
+++ hackathon/generate-extras.sh
@@ -9,9 +9,9 @@ set -eu
                 --library-path-dependency=codeql-javascript
 
 # Convert to csv
-/opt/hostedtoolcache/CodeQL/0.0.0-20201106/x64/codeql/codeql bqrs decode --format=csv scratch/database/results/hackathon/LinesOfCode.bqrs > hackathon/LinesOfCode.csv
-/opt/hostedtoolcache/CodeQL/0.0.0-20201106/x64/codeql/codeql bqrs decode --format=csv scratch/database/results/hackathon/LinesOfComment.bqrs > hackathon/LinesOfComment.csv
-/opt/hostedtoolcache/CodeQL/0.0.0-20201106/x64/codeql/codeql bqrs decode --format=csv scratch/database/results/hackathon/RemoteFlowSources.bqrs > hackathon/RemoteFlowSources.csv
+/opt/hostedtoolcache/CodeQL/0.0.0-20201106/x64/codeql/codeql bqrs decode --format=csv hackathon/LinesOfCode.bqrs > hackathon/LinesOfCode.csv
+/opt/hostedtoolcache/CodeQL/0.0.0-20201106/x64/codeql/codeql bqrs decode --format=csv hackathon/LinesOfComment.bqrs > hackathon/LinesOfComment.csv
+/opt/hostedtoolcache/CodeQL/0.0.0-20201106/x64/codeql/codeql bqrs decode --format=csv hackathon/RemoteFlowSources.bqrs > hackathon/RemoteFlowSources.csv
 
 # Merge results into sarif
 ./hackathon/script.py --sarif /home/runner/work/juice-shop/results/javascript-builtin.sarif --output /home/runner/work/juice-shop/results/javascript-builtin.sarif
