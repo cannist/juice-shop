1051b904308dceac5ce4f960f07df2b28b5fb3b4
Thomas Horstmeyer <cannist@github.com>
2020-12-11 15:54:27 +0000

    Use correct paths, for real

--- hackathon/generate-extras.sh
+++ hackathon/generate-extras.sh
@@ -8,13 +8,11 @@ set -eu
                 ./hackathon/RemoteFlowSources.ql \
                 --library-path-dependency=codeql-javascript
 
-# Find bqrs files
-find / | grep bqrs
 
 # Convert to csv
-/opt/hostedtoolcache/CodeQL/0.0.0-20201106/x64/codeql/codeql bqrs decode --format=csv hackathon/LinesOfCode.bqrs > hackathon/LinesOfCode.csv
-/opt/hostedtoolcache/CodeQL/0.0.0-20201106/x64/codeql/codeql bqrs decode --format=csv hackathon/LinesOfComment.bqrs > hackathon/LinesOfComment.csv
-/opt/hostedtoolcache/CodeQL/0.0.0-20201106/x64/codeql/codeql bqrs decode --format=csv hackathon/RemoteFlowSources.bqrs > hackathon/RemoteFlowSources.csv
+/opt/hostedtoolcache/CodeQL/0.0.0-20201106/x64/codeql/codeql bqrs decode --format=csv /home/runner/work/_temp/codeql_databases/javascript/results/hackathon/LinesOfCode.bqrs > hackathon/LinesOfCode.csv
+/opt/hostedtoolcache/CodeQL/0.0.0-20201106/x64/codeql/codeql bqrs decode --format=csv /home/runner/work/_temp/codeql_databases/javascript/results/hackathon/LinesOfComment.bqrs > hackathon/LinesOfComment.csv
+/opt/hostedtoolcache/CodeQL/0.0.0-20201106/x64/codeql/codeql bqrs decode --format=csv /home/runner/work/_temp/codeql_databases/javascript/results/hackathon/RemoteFlowSources.bqrs > hackathon/RemoteFlowSources.csv
 
 # Merge results into sarif
 ./hackathon/script.py --sarif /home/runner/work/juice-shop/results/javascript-builtin.sarif --output /home/runner/work/juice-shop/results/javascript-builtin.sarif
